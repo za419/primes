e12a359b6d4054fe518d45511f740d41a7f5c0d9
Ryan Hodin <ryan951357@gmail.com>
2017-10-01 02:13:33 -0500

    Add primesBetween argument handling

--- primesBetween/primesBetween.cpp
+++ primesBetween/primesBetween.cpp
@@ -1,10 +1,32 @@
 // primesBetween.cpp : Defines the entry point for the console application.
 //
 
+#include "../primes.h"
 
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
 
-int main()
-{
+void usage(const char* invoke) {
+	std::cout << "Usage: " << invoke << " min max.\n";
+	std::cout << "Prints primes between min and max to stdout (inclusive at both ends).\n";
+	std::cout << "min must be at least 2.\n";
+	std::cout << "max must be at least min. If it isn't, min and max will be swapped.\n";
+	std::exit(1);
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		usage(argv[0]);
+	}
+	uint min = sstream_cast<uint>(argv[1]);
+	if (min < 2) {
+		usage(argv[0]);
+	}
+	uint max = sstream_cast<uint>(argv[2]);
+	if (min > max) {
+		std::swap(min, max);
+	}
     return 0;
 }
 
