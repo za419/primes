8e5c9aa33cb2f486bedfed8ef8c70c2e47ddd276
Ryan Hodin <ryan951357@gmail.com>
2017-09-30 23:17:39 -0500

    Add code for actually computing requested primes

--- primesUntil/primesUntil.cpp
+++ primesUntil/primesUntil.cpp
@@ -52,6 +52,28 @@ int main(int argc, char* argv[]) {
 	// Seed factors are all primes less than 100.
 	std::vector<uint> factors({ 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,
 		53,59,61,67,71,73,79,83,89,97 });
+
+	// Check to see if all the requested primes are precomputed
+	if (max < 97) {
+		// If so, output all primes up to max
+		for (size_t i = 0; factors[i] < max; ++i) {
+			std::cout << factors[i] << '\n';
+		}
+	}
+	else {
+		// Otherwise, start actually computing (after printing all the precomputeds)
+		for (size_t i = 0; i < factors.size(); ++i) {
+			std::cout << factors[i] << '\n';
+		}
+
+		// Compute and print prime numbers
+		for (uint n = 99; n <= max; n += 2) {
+			if (isRelativelyPrime(n, factors)) {
+				std::cout << n << '\n';
+				factors.push_back(n);
+			}
+		}
+	}
     return 0;
 }
 
